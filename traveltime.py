fbb585fbd6d44d327b8daf5291ef71d7eb9643a4
ccmelo <carlmellow@gmail.com>
2016-02-09 23:09:41 -0500

    change structures to use more functions

--- traveltime.py
+++ traveltime.py
@@ -10,9 +10,13 @@ data=pandas.read_csv(filename)
 #mode=driving 
 
 print data 
+data['duration']=" "
+print data 
 
-def gettraveltime(property):
+def makerequests(property):
+     print "in make requests"
      print property
+     print "here"
      origin=str(property['Latitude'])+","+str(property['Longitude'])
      destination="42.3587543,-71.060423"
      print origin 
@@ -20,21 +24,19 @@ def gettraveltime(property):
      p={'key':'AIzaSyBaFlG4WjXyTu1_zy04IaIgbB-01OXPz1g', 'origins': origin, 'destinations': destination}
      r= requests.post('https://maps.googleapis.com/maps/api/distancematrix/json', params=p)
      print r.url
-     a=r.json()
-     print a
-     duration= pandas.Series(a['rows'][0]['elements'][0]['duration']['value'], index=['duration'])
-     property=pandas.concat([property,duration],axis=0)
-     return property 
+     return r.json()
+
+def getduration(traveltime):
+     return traveltime['rows'][0]['elements'][0]['duration']['value']
+
      
-result=pandas.DataFrame()     
-for x in (0,len(data.index)):
+
+for x in range(0,len(data.index)):
     print x 
-    newrow=gettraveltime(data.iloc[x])
-    if x==0:
-        result.loc['0']=pandas.DataFrame(newrow)
-    else:
-        pandas.concat([result,newrow])
-    print "FUNCTION RESULT"
-    print newrow
-    print result
+    ttjson=makerequests(data.iloc[x]) 
+    print ttjson 
+    print data.loc[x]
+    print data['duration']
+    data.ix[x,'duration']=getduration(ttjson)
+    print data 
 #data.apply(gettraveltime, axis=1)
