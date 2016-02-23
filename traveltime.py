a263923081fd2431982928bc2775f212bf032942
Carlota Melo <cmelo@us.costar.local>
2016-02-23 12:35:07 -0500

    add function for input of file location and travel

--- traveltime.py
+++ traveltime.py
@@ -1,30 +1,29 @@
+
 import numpy as np
 import pandas
 import os 
 import requests 
 #import json
 
-os.chdir('C:/Users/cmelo/Documents/Travel Time')
-filename= 'sampledata.csv'
-data=pandas.read_csv(filename)
+
 #mode=driving 
 
 
-data['duration']=" "
-data['distance']=" "
  
+def get_file_location(): 
+    a=raw_input("Where is your csv file located? (I need the full file path please!")
+    return a
+
+def get_mode(): 
+    a=raw_input("What mode of travel are you calculating travel times for? \
+        \n enter D for driving, W for walking, or T for public transit") 
+    return a 
 
 def makerequests(property):
-     print "in make requests"
-     print property
-     print "here"
      origin=str(property['Latitude'])+","+str(property['Longitude'])
      destination="42.3587543,-71.060423"
-     print origin 
-     print destination
      p={'key':'AIzaSyBaFlG4WjXyTu1_zy04IaIgbB-01OXPz1g', 'origins': origin, 'destinations': destination}
      r= requests.post('https://maps.googleapis.com/maps/api/distancematrix/json', params=p)
-     print r.url
      return r.json()
 
 def getduration(traveltime):
@@ -34,11 +33,24 @@ def getduration(traveltime):
 def getdistance(traveltime):
      return traveltime['rows'][0]['elements'][0]['distance']['value']
 
-for x in range(0,len(data.index)):
-    print x 
-    ttjson=makerequests(data.iloc[x]) 
-    data.ix[x,'duration']=getduration(ttjson)
-    data.ix[x,'distance']=getdistance(ttjson)
 
-#data.apply(gettraveltime, axis=1)
+
+file_location=get_file_location()
+filename=file_location.split('\\')[-1]
+file_location='\\'.join(file_location.split('\\')[0:-1])
+
+
+
+os.chdir(file_location)
+data=pandas.read_csv(filename)
+data['duration']=" "
+data['distance']=" "
+mode=get_mode
+
+#for x in range(0,len(data.index)):
+    #ttjson=makerequests(data.iloc[x]) 
+    #data.ix[x,'duration']=getduration(ttjson)
+   # data.ix[x,'distance']=getdistance(ttjson)
+
+
 data.to_csv('sampleoutput.csv')
